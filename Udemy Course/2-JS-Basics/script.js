1bd7e517ff209794afaa666b9cf0667675eb5ef3
ToriK17 <vfcad@protonmail.com>
2019-12-04 12:57:49 -0800

    adds JS practice

--- Udemy Course/2-JS-Basics/script.js
+++ Udemy Course/2-JS-Basics/script.js
@@ -1,34 +1,34 @@
-var firstName = 'Smitty';
-console.log(firstName);
+// var firstName = 'Smitty';
+// console.log(firstName);
 
 
-var age = 28;
-var fullAge = true;
-console.log(fullAge);
+// var age = 28;
+// var fullAge = true;
+// console.log(fullAge);
 
-var job;
-console.log(job);
+// var job1;
+// console.log(job1);
 // not assigning a variable makes this undefined
 
-job = 'Teacher';
-console.log(job);
+// job1 = 'Teacher';
+// console.log(job1);
 
 // variables: letter, $, or _
-var _3years = 3;
-var $2years = 2;
+// var _3years = 3;
+// var $2years = 2;
 
 // Variable mutation and type coercion 
 
 // Type coercion  
-console.log(firstName + ' ' + age);
+// console.log(firstName + ' ' + age);
 
 
 
-var job, isMarried;
-job = 'teacher';
-isMarried = false;
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
 
-console.log(firstName + ' is a ' + age + 'year old ' + job + '. Is he married? ' + isMarried); 
+// console.log(firstName + ' is a ' + age + 'year old ' + job + '. Is he married? ' + isMarried); 
 
 // variable mutation
 
@@ -44,45 +44,191 @@ console.log(firstName + ' is a ' + age + 'year old ' + job + '. Is he married? '
 */
 
 // typeof operator
-console.log(typeof age);
-console.log(typeof isMarried);
+// console.log(typeof age);
+// console.log(typeof isMarried);
 
 // operator order  
 
-var now = 2019;
-var yearJohn = 1989;
-var legalAge = 21;
+// var now = 2019;
+// var yearJohn = 1989;
+// var legalAge = 21;
 
-var isLegalAge = now - yearJohn >= legalAge;
-console.log(isLegalAge);
+// var isLegalAge = now - yearJohn >= legalAge;
+// console.log(isLegalAge);
 
 // here assignment works from right to left, thats why this works
-var x, y;
-x = y = (3 + 5) * 5 - 6;
-console.log(x, y);
+// var x, y;
+// x = y = (3 + 5) * 5 - 6;
+// console.log(x, y);
 
 // challenge
 
 // Store both mass and height in variables
-var markMass = 167;
-var johnMass = 148;
-var markHeight = 177;
-var johnHeight = 155;
+// var markMass = 167;
+// var johnMass = 148;
+// var markHeight = 177;
+// var johnHeight = 155;
 
 // calculate both BMI
-var markBmi = markMass / (markHeight *markHeight);
-var johnBmi = johnMass / (johnHeight * johnHeight);
+// var markBmi = markMass / (markHeight *markHeight);
+// var johnBmi = johnMass / (johnHeight * johnHeight);
 
 // create a boolean variable to show if Mark has a higher BMI than john 
 
 
-var markHigherBmi;
+// var markHigherBmi;
 
-if (markBmi > johnBmi) {
-  markHigherBmi =  true;
-} else {
-  markHigherBmi = false;
-}
+// if (markBmi > johnBmi) {
+//   markHigherBmi =  true;
+// } else {
+//   markHigherBmi = false;
+// }
 
 // print string stating boolean above
-console.log("Is Mark's BMI higher than John's?" + "it is "+ markHigherBmi);
+// console.log("Is Mark's BMI higher than John's?" + "it is "+ markHigherBmi);
+
+// another way to solve
+// var markHighestBMI =  markBmi > johnBmi;
+// console.log("Is Mark's BMI higher than John's?" + "it is "+ markHighestBMI);
+
+/*************************
+* if/else statements
+*/
+
+// var name = 'Bob';
+// var civilStatus = 'single';
+
+// if (civilStatus === 'married') {
+//   console.log(name + 'is married');
+// } else {
+//   console.log(name + 'not married');
+// }
+
+// dont need to write isMarried === true as condition because it is a boolean, this is enough for the condition
+// var isMarried = true;
+
+// if (isMarried) {
+//   console.log(name + 'is married');
+// } else {
+//   console.log(name + 'is not married');
+// }
+
+/*******************
+* Boolean logic
+*/
+
+// var firstName = 'Jim';
+// var age = 17;
+
+// if (age < 13) {
+//   console.log(firstName + 'is a boy.');
+// } else if (age >= 13 && age < 19) {
+//   console.log(firstName + 'is a teen');
+// } else {
+//   console.log(firstName + 'is a man.');
+// }
+
+/********************* 
+* The Ternary Operator and Switch statements
+*/
+
+// var firstName = 'John';
+// var age = 16;
+
+// Ternary Operator   ? : ;
+// age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+// var drink = age >= 18 ? 'beer' : 'juice';
+// console.log(drink);
+
+// Switch statement
+// var job = 'teacher';
+// switch (job) {
+//   case 'teacher':
+//   case 'instructor':
+//     console.log(firstName + 'teaches to code.');
+//     break;
+//   case 'driver':
+//     console.log(firstName + 'drives for Uber');
+//     break;
+//   case 'designer': 
+//     console.log(firstName + 'designs websites');
+//     break;
+//   default: 
+//     console.log(firstName + 'does somethig else.');      
+// }
+
+// Another switch example
+// age = 12;
+
+// switch (true) {
+//   case age < 13:
+//     console.log(firstName + ' is a boy.');
+//     break;
+//   case age >= 13 && age < 20:
+//     console.log(firstName + 'is a teenager');
+//     break;  
+//   default:
+//     console.log(firstName + ' is a man'); 
+// }
+
+
+
+/**********************************
+* Truthy and Falsy values and equality operators 
+*/
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+// var height;
+
+// if (height || height === 0 ) {
+//   console.log('variable is defined');
+// } else {
+//   console.log('Variable has NOT been defined');
+// }
+
+// Equality Operators: == does type coersion, so '23' == 23 is TRUE but with triple equals '23' === 23 is FALSE
+
+
+
+/* Coding Chalenge 2 */ 
+
+// 1. Caluclate the average score for each team
+var johnTeamAvg = (89 + 120 + 103) / 3;
+var mikeTeamAvg = (116 + 94 + 123) / 3;
+
+// 2. Decide which team wins and print to console
+
+if (johnTeamAvg > mikeTeamAvg) {
+  console.log('john\'s team wins');
+} else {
+  console.log('mike\'s team wins');
+}
+
+// 3. take in account the option of a draw
+johnTeamAvg = 12;
+mikeTeamAvg = 12;
+
+if (johnTeamAvg === mikeTeamAvg) {
+  console.log('They draw, no one wins');
+} else if (johnTeamAvg > mikeTeamAvg) {
+  console.log('john\'s team wins');
+} else {
+  console.log('mike\'s team wins');
+} 
+
+// 4. 
+var maryTeamAvg = (97 + 134 + 105) / 3;
+maryTeamAvg = 12
+
+if (johnTeamAvg === mikeTeamAvg && maryTeamAvg === mikeTeamAvg) {
+  console.log('They draw, no one wins');
+} else if (johnTeamAvg > mikeTeamAvg && johnTeamAvg > maryTeamAvg) {
+  console.log('john\'s team wins');
+} else if (mikeTeamAvg > johnTeamAvg && mikeTeamAvg > maryTeamAvg) {
+  console.log('mike\'s team wins');
+} else if (maryTeamAvg > johnTeamAvg && maryTeamAvg > mikeTeamAvg) {
+  console.log('mary\'s team wins');
+}
