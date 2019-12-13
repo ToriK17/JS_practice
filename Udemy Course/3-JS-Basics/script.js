f8ba58eb68760ebcbb225c986050599f7d4c043e
ToriK17 <vfcad@protonmail.com>
2019-12-13 12:30:36 -0800

    adds start for game

--- Udemy Course/3-JS-Basics/script.js
+++ Udemy Course/3-JS-Basics/script.js
@@ -1,38 +1,40 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
-calculateAge(1965);
+// calculateAge(1965);
 // Used before declaring * Most important use case, using functions before declared *
 
-function calculateAge(year) {
-  console.log(2016 - year);
-}
+// function calculateAge(year) {
+//   console.log(2016 - year);
+// }
 
-calculateAge(1990);
+// calculateAge(1990);
 
 
 //            With functions that are expressions:
 // retirement(1960); This doesnt work with hoisting
-var retirement = function(year) {
-  console.log(65 - (2016 - year));
-};
+// var retirement = function(year) {
+//   console.log(65 - (2016 - year));
+// };
 
-retirement(1990);
+// retirement(1990);
 
 //               Variables:
-console.log(age); 
+// console.log(age); 
 //  Variables are scanned with hoisting but set to UNDEFINED
-var age  = 23;
+// var age  = 23;
 // Now it reads as defined
-console.log(age);
+// console.log(age);
 
-function foo() {
-  var age = 65;
-  console.log(age);
-}
-foo();
-console.log(age);
+// function foo() {
+//   var age = 65;
+//   console.log(age);
+// }
+// foo();
+// console.log(age);
 // Returns the global execution object 23, the function did not chaneg global scope
+
+// To save memory we can make our functions expressions by adding var before them and then they will not be hoisted
 
 
 ///////////////////////////////////////
@@ -73,10 +75,23 @@ function third() {
     var d = 'John';
     console.log(a + b + c + d);
 }
-*/
+*/ 
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+function calculateAge(year) {
+  console.log(2019 - year);
+}
+
+var john = {
+  name: 'john', 
+  year: 1990, 
+  age: calculateAge(1990)
+};
+
+console.log(john);
+
+// note: figure out why scope rules do not apply here
