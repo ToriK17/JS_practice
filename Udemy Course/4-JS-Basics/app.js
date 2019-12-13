89a3f2cd717a82767fe931679abfb096c1e7226f
ToriK17 <vfcad@protonmail.com>
2019-12-13 15:24:55 -0800

    adds finalized piggy game

--- Udemy Course/4-JS-Basics/app.js
+++ Udemy Course/4-JS-Basics/app.js
@@ -2,63 +2,64 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
+- In each turn, a player rolls a dice as many times as he wishes. Each result get added to their round score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 init();
 
 // dice = Math.floor(Math.random() * 6 ) + 1;
-
 //         querySelector selects values and elements just like we do in CSS
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 //         using querySelector to manipulate the DOM via HTML
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 
-
-
-
 // we add a anonoymous function below in the addEventListener method. One without a name that cannot be reused outside this method
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  // 1. Random number 
-  var dice = Math.floor(Math.random() * 6) + 1;
+  if (gamePlaying) {
+    // 1. Random number 
+    var dice = Math.floor(Math.random() * 6) + 1;
 
-  // 2. Display the result
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.diplay = 'block';
-  diceDOM.src = 'dice-' + dice + '.png';
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.diplay = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
-  // 3. Update the round score IF the rolled number was NOT a 1 
-  if (dice !== 1) {
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-  } else {
-    // Next Player
-    nextPlayer();
+    // 3. Update the round score IF the rolled number was NOT a 1 
+    if (dice !== 1) {
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      // Next Player
+      nextPlayer();
+    }
   }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  // Adds Current score to global score
-  scores[activePlayer] += roundScore;
+  if (gamePlaying) {
+    // Adds Current score to global score
+    scores[activePlayer] += roundScore;
 
-  // update th UI
-  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    // update th UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-  // Check if player won the game
-  if (scores[activePlayer] >= 10) {
-    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-    document.querySelector('.dice').style.display = 'none';
-    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-  } else {
-    nextPlayer();
-  }
+    // Check if player won the game
+    if (scores[activePlayer] >= 10) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
+  } 
 });
 
 
@@ -82,6 +83,7 @@ function init() {
   scores = [0,0];
   roundScore = 0;
   activePlayer = 0;
+  gamePlaying = true;
 
   document.querySelector('.dice').style.display = 'none';
 
